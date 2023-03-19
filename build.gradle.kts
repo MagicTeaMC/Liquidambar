84b6b72d9f48e3b5c8d8d3cab3ca11fac8764fa2
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-03-19 08:15:35 +0800

    Build changes

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+import io.papermc.paperweight.util.constants.*
+
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.4-SNAPSHOT"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        content { onlyForConfigurations("paperclip") }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("org.quiltmc:quiltflower:1.9.0")
+    paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+        maven("https://repo.codemc.io/repository/maven-public/")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":liquidambar-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://maven.quiltmc.org/")
+
+    useStandardUpstream("purpur") {
+        url.set(github("PurpurMC", "Purpur"))
+        ref.set(providers.gradleProperty("purpurRef"))
+
+        withStandardPatcher {
+            baseName("Purpur")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+
+            apiOutputDir.set(layout.projectDirectory.dir("liquidambar-api"))
+            serverOutputDir.set(layout.projectDirectory.dir("liquidambar-server"))
+        }
+    }
+}
